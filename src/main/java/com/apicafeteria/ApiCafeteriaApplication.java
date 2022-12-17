5fc061003b2316813ca4a76f995b1011508b7ae5
AndersonElit <rodriarri@gmail.com>
2022-12-17 11:43:26 -0500

    incorporacion de swagger 3

--- src/main/java/com/apicafeteria/ApiCafeteriaApplication.java
+++ src/main/java/com/apicafeteria/ApiCafeteriaApplication.java
@@ -1,9 +1,11 @@
 package com.apicafeteria;
 
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPI30
 public class ApiCafeteriaApplication {
 
 	public static void main(String[] args) {
